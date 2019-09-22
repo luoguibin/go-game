26583450dc60e86415cc668ec98522a2ebeb5c02
Sghen <945654877@qq.com>
2019-09-22 23:45:54 +0800

    [*]修复远程IP获取

--- game/game-server.go
+++ game/game-server.go
@@ -5,6 +5,7 @@ import (
 	"go-game/helper"
 	"go-game/models"
 	"net/http"
+	"strings"
 	"sync"
 
 	"github.com/astaxie/beego/context"
@@ -53,7 +54,7 @@ func AddToServer(Ctx *context.Context, ID int64) {
 	if err != nil {
 		models.MConfig.MLogger.Error("get ws error:\n%s", err)
 	}
-	models.MConfig.MLogger.Debug("get ws: " + ws.RemoteAddr().String())
+	models.MConfig.MLogger.Debug("get ws: " + getCurrentIP(Ctx.Request))
 
 	gameClient := &GameClient{
 		ID:   ID,
@@ -69,6 +70,17 @@ func AddToServer(Ctx *context.Context, ID int64) {
 	MGameServer.clientMap.Store(gameClient.ID, gameClient)
 
 	go GoGameClientHandle(gameClient)
+}
+
+func getCurrentIP(r *http.Request) string {
+	// 这里也可以通过X-Forwarded-For请求头的第一个值作为用户的ip
+	// 但是要注意的是这两个请求头代表的ip都有可能是伪造的
+	ip := r.Header.Get("X-Real-IP")
+	if ip == "" {
+		// 当请求头不存在即不存在代理时直接获取ip
+		ip = strings.Split(r.RemoteAddr, ":")[0]
+	}
+	return ip
 }
 
 // GoGameClientHandle ...
